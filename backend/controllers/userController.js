6c45fe19015c39c9e17c6ecc7aff20c894b9aa04
Aviatorscode2 <victoryakubu5@gmail.com>
2022-09-13 02:42:37 +0100

    error and exception handling

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,25 @@
-const registerUser = (req, res) => {
-    res.send("Register User");
-}
+const asyncHandler = require('express-async-handler');
+// @desc Register a new user
+// @route /api/users
+// @access Public
+const registerUser = asyncHandler(async(req, res) => {
+    const {name, email, password} = req.body;
 
-const loginUser = (req, res) => {
+    //Simple validation
+    if(!name || !email || !password) {
+        res.status(400)
+        throw new Error('Please enter all fields');
+    }
+
+    res.send("Register User");
+})
+
+// @desc Login a user
+// @route /api/users/login
+// @access Public
+const loginUser = asyncHandler(async(req, res) => {
     res.send("Login User");
-}
+})
 
 module.exports = {
     registerUser,
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,11 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 const app = express();
+const {errorHandler} = require('./middleware/errorMiddleware');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 
 // Creating a route with express
 app.get('/', (req, res) => {
@@ -12,6 +17,7 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/users', require('./routes/userRoutes'));
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
